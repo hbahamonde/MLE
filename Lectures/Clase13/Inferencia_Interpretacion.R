b5fd576cc1f1a8d8c42214b29b15a134d63fe8bc
Hector <bahamondeh@gmail.com>
2020-11-16 16:27:19 -0300

    lecture

--- Lectures/Clase13/Inferencia_Interpretacion.R
+++ Lectures/Clase13/Inferencia_Interpretacion.R
@@ -1,7 +1,7 @@
 ### R code from vignette source 'Inferencia_Interpretacion.Rnw'
 
 ###################################################
-### code chunk number 1: setup20
+### code chunk number 1: setup
 ###################################################
 if (!require("pacman")) install.packages("pacman"); library(pacman)
 p_load(knitr)
@@ -32,17 +32,25 @@ confint(logit.1)
 
 
 ###################################################
-### code chunk number 5: od:ra
+### code chunk number 5: ci:plot
+###################################################
+p_load(jtools,ggstance,broom.mixed)
+plot_summs(logit.1)
+
+
+###################################################
+### code chunk number 6: od:ra
 ###################################################
 p_load(oddsratio)
 #
-or_glm(data = mydata, 
+odds = or_glm(data = mydata, 
        model = logit.1, 
-       incr = list(gre = 380, gpa = 5))
+       incr = list(gre = mean(mydata$gre), gpa = mean(mydata$gpa) + sd(mydata$gpa)))
+odds
 
 
 ###################################################
-### code chunk number 6: od:plot
+### code chunk number 7: od:plot
 ###################################################
 par(mfrow=c(1,2))
 hist(mydata$gpa, main = "GPA")
@@ -50,14 +58,14 @@ hist(mydata$gre, main = "GRE")
 
 
 ###################################################
-### code chunk number 7: means
+### code chunk number 8: means
 ###################################################
 mean(mydata$gpa)
 mean(mydata$gre)
 
 
 ###################################################
-### code chunk number 8: sc1
+### code chunk number 9: sc1
 ###################################################
 p_load(margins)
 summary(margins(logit.1, 
@@ -68,44 +76,57 @@ summary(margins(logit.1,
 
 
 ###################################################
-### code chunk number 9: sc2
+### code chunk number 10: sc2
 ###################################################
 summary(margins(logit.1, 
         at = list(
           gre = max(mydata$gre), 
-          gpa = min(mydata$gpa):mean(mydata$gpa))
+          gpa = mean(mydata$gpa))
           ))
 
 
 ###################################################
-### code chunk number 10: sc3
+### code chunk number 11: sc3
 ###################################################
 par(mfrow=c(1,2))
-cplot(logit.1, "gre")
-cplot(logit.1, "gpa")
+cplot(logit.1, "gre", what = "effect") # effect para mostrar cambios marginales
+cplot(logit.1, "gpa", what = "effect") # effect para mostrar cambios marginales
 
 
 ###################################################
-### code chunk number 11: predict:1
+### code chunk number 12: predict:1
 ###################################################
 head(predict(logit.1))
+summary(predict(logit.1))
 plot(predict(logit.1)) # logit scale
-plot(predict(logit.1, type="response")) # Predicted Prob
 
 
 ###################################################
-### code chunk number 12: predict:2
+### code chunk number 13: logit:prob:1
+###################################################
+logit.scale = as.numeric(predict(logit.1)) # extraer vector y'
+prob.scale = exp(logit.scale)/(1+exp(logit.scale)) # exponenciar
+head(prob.scale) # ve como se ve y' en formato prob
+summary(prob.scale) # resumen de y' en formato prob (bounded)
+
+
+###################################################
+### code chunk number 14: logit:prob:2
+###################################################
+par(mfrow=c(1,2)) # divide el panel de graf's en dos
+plot(mydata$gre, prob.scale) # para gre
+plot(mydata$gpa, prob.scale) # para gpa
+
+
+###################################################
+### code chunk number 15: predict:2
 ###################################################
 p_load("effects")
-par(mfrow=c(1,1))
-plot(predictorEffects(logit.1)) # Todo el modelo (PLURAL)
-par(mfrow=c(1,2))
-plot(predictorEffect("gre", logit.1)) # Solo una variable (SINGULAR)
-plot(gpa.pred.prob <- predictorEffect("gpa", logit.1)) # Solo una variable (SINGULAR)
+plot(predictorEffects(logit.1)) # Todo el modelo 
 
 
 ###################################################
-### code chunk number 13: tabla:1
+### code chunk number 16: tabla:1
 ###################################################
 # generar los rangos de los perfiles deseados
 gpa.bajo <- with(mydata, 
@@ -126,16 +147,16 @@ gpa.alto <- with(mydata,
 
 
 ###################################################
-### code chunk number 14: tabla:2
+### code chunk number 17: tabla:2
 ###################################################
 # usar funcion predict
-predict(logit.1, gpa.bajo, type="response")
-predict(logit.1, gpa.medio, type="response")
-predict(logit.1, gpa.alto, type="response")
+predict(logit.1, gpa.bajo, type="response") # gpa.bajo
+predict(logit.1, gpa.medio, type="response") # gpa.medio
+predict(logit.1, gpa.alto, type="response") # gpa.alto
 
 
 ###################################################
-### code chunk number 15: archivo
+### code chunk number 18: archivo
 ###################################################
 knitr::purl('Inferencia_Interpretacion.Rnw')
 Stangle('Inferencia_Interpretacion.Rnw')
